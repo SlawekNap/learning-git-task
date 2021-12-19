5da020fca8c69adad5dd89580165be25f7b4f967
Slawomir Napiorkowski <napiorkowski.slawomir@gmail.com>
2021-12-19 11:48:08 +0100

    dodanie ostaniego zadania i funkcji suma

--- Init_project.py
+++ Init_project.py
@@ -8,3 +8,5 @@ słownik = {
     }
 for sklep in słownik:
 print(f'Idę do {sklep}, kupuję tu następujące {słownik.get(sklep)}'),
+Ilość_zakupów_w_sklepie = (len(słownik.get("Piekarnia")))+(len(słownik.get("Warzywniak")))
+print(f"W sumie kupuję {Ilość_zakupów_w_sklepie} produktów.")
