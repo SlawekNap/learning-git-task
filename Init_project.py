23daa0eaf44ebfb548b4fee03f5dea5cd1893f56
Slawomir Napiorkowski <napiorkowski.slawomir@gmail.com>
2021-12-19 11:53:54 +0100

    wyboldowanie nazw sklepów oraz nazw towarów

--- Init_project.py
+++ Init_project.py
@@ -3,10 +3,10 @@ print("Zadanie 1")
 print("Lista zakupów")
 
 słownik = {
-    "Piekarnia": ["Chleb", "Pączek", "Bułki"], 
-    "Warzywniak": ["Marchew", "Seler", "Rukola"]
+    "Piekarnia": ["Chleb".upper(), "Pączek".upper(), "Bułki".upper()], 
+    "Warzywniak": ["Marchew".upper(), "Seler".upper(), "Rukola".upper()]
     }
 for sklep in słownik:
-print(f'Idę do {sklep}, kupuję tu następujące {słownik.get(sklep)}'),
+    print(f'Idę do {sklep.upper()}, kupuję tu następujące {słownik.get(sklep)}'),
 Ilość_zakupów_w_sklepie = (len(słownik.get("Piekarnia")))+(len(słownik.get("Warzywniak")))
 print(f"W sumie kupuję {Ilość_zakupów_w_sklepie} produktów.")
