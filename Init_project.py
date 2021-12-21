43517092e10da105f19533c3922a595e1e887a2c
Slawomir Napiorkowski <napiorkowski.slawomir@gmail.com>
2021-12-21 12:36:16 +0100

    Zyczenia

--- Init_project.py
+++ Init_project.py
@@ -1,24 +1,3 @@
-
-print("Zadanie 1")
-print("Lista zakupów")
-
-słownik = {
-    "Piekarnia": ["Chleb".upper(), "Pączek".upper(), "Bułki".upper()], 
-    "Warzywniak": ["Marchew".upper(), "Seler".upper(), "Rukola".upper()]
-    }
-for sklep in słownik:
-    print(f'Idę do {sklep.upper()}, kupuję tu następujące {słownik.get(sklep)}'),
-Ilość_zakupów_w_sklepie = (len(słownik.get("Piekarnia")))+(len(słownik.get("Warzywniak")))
-print(f"W sumie kupuję {Ilość_zakupów_w_sklepie} produktów.")
-
-print("")
-print("commit nr 1 na Git Hub")
-for x in range(1,100):
-    if x >= 80:
-        print(x)
-print("")
-print("2 komit do zadnia")
-list = {1:100}
-for x in list:
-    print(x)
-
+print("Idzie kurczak, baran, zając w łapkach wielki kosz trzymając,a w tym koszu, aż po brzegi ogrom życzeń dla kolegi!")
+print("Dużo szczęścia i radości a na stole wspaniałości; kiełbas, jajek i mazurka, babek słodkich i ogórkamna zagrychę do wódeczki")
+print("kiedy pójdą w ruch szklaneczki. Niech te Święta Wielkanocne będą miłe i radosne, a na leja, według wskazań  złap za wiadro i naparzaj!")
